29102e2478cdd7aa06259a7b3eb6b702f0c6e8c0
rodrigo <jrodrigo0592@gmail.com>
2022-07-11 18:35:36 -0300

    finalizando controller da aplicação

--- src/movies/controllers/movies.controller.ts
+++ src/movies/controllers/movies.controller.ts
@@ -7,31 +7,36 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { CreateMovieDTO } from '../dtos/CreateMovieDTO';
+import { UpdateMovieDTO } from '../dtos/UpdateMovieDTO';
+import { MoviesService } from '../services/movies/movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private moviesServices: MoviesService) {}
+
   @Get()
   findAll() {
-    return [1, 2, 3];
+    return this.moviesServices.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
-    return id;
+    return this.moviesServices.findOne(id);
   }
 
   @Post()
-  create(@Body() body: any) {
-    return body;
+  create(@Body() movie: CreateMovieDTO) {
+    return this.moviesServices.create(movie);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() body: any) {
-    return body;
+  update(@Param('id') id: number, @Body() movie: UpdateMovieDTO) {
+    return this.moviesServices.update(id, movie);
   }
 
   @Delete(':id')
-  delete() {
-    return true;
+  delete(@Param('id') id: number) {
+    this.moviesServices.delete(id);
   }
 }
